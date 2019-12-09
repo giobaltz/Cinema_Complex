91dd88c5f391c1c0317c9ce86347d9a2609763a0
Χρήστος Γιαννίκης <xrist.giann@gmail.com>
2019-12-09 18:36:28 +0200

    add reseption image

--- Cinema_Complex/Properties/Resources.Designer.cs
+++ Cinema_Complex/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace Cinema_Complex.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap reseption2 {
+            get {
+                object obj = ResourceManager.GetObject("reseption2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
